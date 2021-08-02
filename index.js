fc878015b6b81dc1f22b7076f909fd30611aa81b
Andita Kaesar <anditakaesar@gmail.com>
2021-08-02 16:51:06 +0700

    update main index

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const app = require('./dist/app').default
 const env = require('./dist/env').default
+const logger = require('./dist/logger').default
 
 app.listen(env.PORT, () => {
-  console.log(env)
+  logger.info('env', process.env.NODE_ENV)
+  if (env.NODE_ENV.includes('dev')) {
+    logger.info('env', env)
+  }
   console.log('app running at http://localhost:8000')
   console.log('CLOSE THIS WINDOW WHEN DONE')
 })
